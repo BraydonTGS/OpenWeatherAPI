551098dfd2e5511e19235a7586af01afb5a192d5
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-10-12 14:01:31 -0500

    finished the 3 api methods, cleaned up my file

--- OpenWeatherAPi/Program.cs
+++ OpenWeatherAPi/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using OpenWeatherAPi.Weather;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
+//Getting all of the information from appsettings // 
+string key = File.ReadAllText("appsettings.json");
+
+//Parsing For the Value of APIKEY //
+string APIKey = JObject.Parse(key).GetValue("APIKEY").ToString();
+
+Console.Write("Please Enter a City Name: ");
+var cityName = Console.ReadLine().ToLower().Trim();
+
+var apiString = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}&units=imperial";
+
+var name = Weather.GetName(apiString);
+var temp = Weather.GetTemp(apiString);
+var weather = Weather.GetWeather(apiString);
+
+Console.WriteLine($"The City of {cityName} has a current temperature of {temp} with {weather}");
+Console.ReadKey();
---
+++ OpenWeatherAPi/Weather/Weather.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherAPi.Weather
+{
+    public class Weather
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+
+        }
+
+        public static string GetName(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var name = JObject.Parse(response)["name"].ToString();
+
+            return name;
+        }
+
+        public static string GetWeather(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var weather = JObject.Parse(response)["weather"][0]["main"].ToString();
+
+            return weather;
+        }
+    }
+}
+
